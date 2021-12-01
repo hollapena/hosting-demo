972ff3e842bbdb71ce2b2f0548c547e3c4a8649e
Callen Schultz <callen.schultz@gmail.com>
2021-12-01 12:14:03 -0600

    trying again

--- server/index.js
+++ server/index.js
@@ -5,9 +5,17 @@ const app = express()
 
 app.get("http://localhost:4000/api/compliment", (req, res) => {
     const compliments = ["Gee, you're a smart cookie!",
-                       "Cool shirt!",
-                       "Your Javascript skills are stellar.",
-    ];
+					 "Cool shirt!",
+					 "Your Javascript skills are stellar.",
+  ];
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+  res.status(200).send(randomCompliment);
+  
+});
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "../index.html"))
